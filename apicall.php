65080e9a6322787f4ad2fa5e7d9ac59a0a0343a9
Alf Müller <31915686+AlfMueller@users.noreply.github.com>
2023-06-30 09:55:40 +0200

    new class added

--- apicall.php
+++ apicall.php
@@ -1,110 +1,178 @@
 <?php
 
-/****************************************************************
-*                                                               *
-* Democode für API Swisspost (PHP V7.4)				*
-*                                                               *
-* Author: Alf Müller - bastelgarage.ch - purecrea gmbh		*
-* Date: 8.2.2021						*
-*                                                               *
-*****************************************************************/
+/*******************************************
+ * Author: Alf Müller (purecrea gmbh)
+ * Date: 2023-06-30
+ 
+ 
+ ZPL file test online: 
+ http://labelary.com/viewer.html (100x150mm 300dpi)
+
+ * This document provides a SwissPostAPI class that allows you to generate an address label barcode
+ * using the Swiss Post API. The class handles the authentication process, sends a POST request to the API,
+ * and retrieves the generated barcode label data in ZPL format. The ZPL string is then displayed and can be
+ * saved as a .zpl file. Additionally, the comment provides instructions on how to test the generated ZPL file
+ * using the Labelary ZPL Viewer and how to send the ZPL file to a Zebra printer for printing the barcode label.
+ *
+ * To use this document, replace the "XXXXX" placeholders with your actual client ID and client secret,
+ * and then create an instance of the SwissPostAPI class to generate the barcode label.
+ */
 
 
- $client_secret = 'TEST_XXXXXXXXX'; 		
- $client_id = 'TEST_XXXXXXXXXXX';
+$client_id          = "XXXXX";
+$client_secret      = "XXXXX";
 
-$scope = "WEDEC_AUTOCOMPLETE_ADDRESS";
-$parameters = "zipCity=4586&type=DOMICILE";
-
-
-// ab hier nichts mehr ändern
-
-//adress
-$authorize_url = "https://wedec.post.ch/WEDECOAuth/authorization";
-$token_url = "https://wedec.post.ch/WEDECOAuth/token";
-
-
-//	Callback-URL, die bei der Definition der Anwendung angegeben wurde - muss mit dem übereinstimmen, was die Anwendung sagt
-$callback_uri = ((empty($_SERVER['HTTPS'])) ? 'http' : 'https') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$test_api_url = "https://wedec.post.ch/api/address/v1/zips?".$parameters;
-
-function getAuthorizationCode() {
-	global $authorize_url, $client_id, $callback_uri;
-
-	$authorization_redirect_url = $authorize_url . "?response_type=code&client_id=" . $client_id . "&redirect_uri=" . $callback_uri . "&scope=".$scope;
-
-	header("Location: " . $authorization_redirect_url);
-}
-//echo $posttoken;
-
-//	Schritt I, J - den Autorisierungscode in ein Zugriffstoken umwandeln, usw.
-function getAccessToken() {
-	global $token_url, $client_id, $client_secret, $callback_uri, $scope;
-
-	//$authorization = base64_encode("$client_id:$client_secret");
-	//$header = array("Authorization: Basic {$authorization}","Content-Type: application/x-www-form-urlencoded");
-	$content = "grant_type=client_credentials&client_id=$client_id&client_secret=$client_secret&redirect_uri=$callback_uri&scope=$scope";
-
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => $token_url,
-		CURLOPT_HTTPHEADER => $header,
-		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_POST => true,
-		CURLOPT_POSTFIELDS => $content,
-		CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "content-type: application/x-www-form-urlencoded"
-            )
-	));
-	$response = curl_exec($curl);
-	curl_close($curl);
-
-	if ($response === false) {
-		echo "Failed";
-		echo curl_error($curl);
-		echo "Failed";
-	} elseif (json_decode($response)->error) {
-		echo "Error:<br />";
-		echo $response;
-	}
-
-	return json_decode($response)->access_token;
-}
-
-$token = getAccessToken();
-//	wir können nun das access_token beliebig oft für den Zugriff auf geschützte Ressourcen verwenden
-function getResource($token) {
-	global $test_api_url;
+class SwissPostAPI {
+    private $apiUrl = 'https://dcapi.apis.post.ch/barcode/v1/generateAddressLabel';
+    private $client_id;
+    private $client_secret;
+    private $accessToken;
+            
+    public function __construct($client_id, $client_secret) {
+        $this->clientId = $client_id ;
+        $this->clientSecret = $client_secret;
+        $this->authenticate();
+    }
+       /**
+     * Authenticates the client using the provided client ID and client secret.
+     */
 	
-$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL =>$test_api_url,
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
-	  CURLOPT_HTTPHEADER => array(
-    	'Authorization: Bearer ' . $token
-  	),
+    private function authenticate() {
+        $tokenUrl = 'https://api.post.ch/OAuth/token';
+        $postData = array(
+            'grant_type' => 'client_credentials',
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'scope' => 'DCAPI_BARCODE_READ' // Replace with the correct scope
+        );
+        
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $tokenUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => http_build_query($postData),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+
+        $responseData = json_decode($response, true);
+
+
+        if (isset($responseData['access_token'])) {
+            $this->accessToken = $responseData['access_token'];
+        } else {
+            // Handle authentication error
+            // You can customize this part based on your needs
+            echo 'Authentication error: ' . $response;
+        }
+    }
+	
+	
+	    /**
+     * Creates a barcode by sending a POST request to the Swiss Post API.
+     */
+    
+    public function createBarcode() {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://dcapi.apis.post.ch/barcode/v1/generateAddressLabel',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS =>'{
+            "language": "DE",
+            "frankingLicense": "60125922",
+            "ppFranking": false,
+            "customer": {
+                "name1": "Purecrea GmbH",
+                "name2": "bastelgarage.ch",
+                "street": "Gewebestrasse 23",
+                "zip": "4556",
+                "city": "Subingen",
+                "country": "CH",
+                "logoRotation": 0,
+                "domicilePostOffice": "4586 Subingen"
+            },
+            "customerSystem": null,
+            "labelDefinition": {
+                "labelLayout": "A6",
+                "printAddresses": "RECIPIENT_AND_CUSTOMER",
+                "imageFileType": "ZPL2",
+                "imageResolution": 300,
+                "printPreview": false
+            },
+            "item": {
+                "itemID": "00000000001000727746",
+                "recipient": {
+                    "title": "Frau",
+                    "name1": "Serena Muster",
+                    "street": "Teststrasse 11",
+                    "mailboxNo": null,
+                    "zip": "9000",
+                    "city": "St.Gallen",
+                    "country": "CH",
+                    "houseKey": "24",
+                    "email": "test@test.test"
+                },
+                "attributes": {
+                    "przl": [
+                        "PRI"
+                    ],
+                    "deliveryDate": null,
+                    "returnInfo": {
+                        "returnNote": false,
+                        "instructionForReturns": false,
+                        "returnService": null,
+                        "customerIDReturnAddress": null
+                    },
+                    "weight": 1
+                }
+            }
+        }',
+          CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $this->accessToken,
+                'Content-Type: application/json'
+            ),
 ));
 
-	$response = curl_exec($curl);
-	curl_close($curl);
-	echo $response;
+$response = curl_exec($curl);
 
+curl_close($curl);
+$labelData = json_decode($response, true);
+$zplString = base64_decode($labelData['item']['label'][0]);
+$zplStringout = str_replace("^", "<br>^", $zplString);
+echo ("^XA". $zplStringout. "^XZ"); 
+$zplString = "^XA". $zplString. "^XZ"; 
+
+// Save ZPL string to a file
+$date = date("Y-m-d");
+$fileName = "barcode_$date.zpl";
+ file_put_contents($fileName, $zplString);
+    }
 }
 
-if(isset($_GET['token'])) {
-	echo getResource($_GET['token']);
-}else{
-	$token = getAccessToken();
-	header('Location: apicall.php?token='.$token);
-}
+
+
+// Example usage
+$api = new SwissPostAPI($client_id, $client_secret);
+$response = $api->createBarcode();
+
 
 ?>
-
